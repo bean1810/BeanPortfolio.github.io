d7afa2616344ab04c40ee357e9a1b6a794a5ac59
Dung <mr.bean5201314@gmail.com>
2018-05-13 22:27:39 +0700

    New UI

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const watch = require('gulp-watch');
 const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
 
 let import_style = {
     bootstrap_css: "node_modules/bootstrap/dist/css/bootstrap.min.css",
@@ -8,7 +9,8 @@ let import_style = {
     jquery: "node_modules/jquery/dist/jquery.min.js",
     bootstrap_js: "node_modules/bootstrap/dist/js/bootstrap.min.js",
     my_js: "assets/js/*.js",
-    font: "assets/fonts/*"
+    font: "assets/fonts/*",
+    index : "index.html"
 };
 
 let export_style = {
@@ -19,23 +21,36 @@ let export_style = {
 gulp.task('css', function () {
     return gulp.src([import_style.bootstrap_css, import_style.my_css])
         .pipe(concat('style.min.css'))
-        .pipe(gulp.dest(export_style.css));
+        .pipe(gulp.dest(export_style.css))
+        .pipe(browserSync.stream());
 });
+
 
 gulp.task('js', function () {
     return gulp.src([import_style.jquery, import_style.bootstrap_js, import_style.my_js])
         .pipe(concat('main.min.js'))
-        .pipe(gulp.dest(export_style.js));
+        .pipe(gulp.dest(export_style.js))
+        .pipe(browserSync.stream());
 });
 
 // gulp.task('font', function () {
 //     return gulp.src([import_style.font])
 //         .pipe(gulp.dest(export_style.font));
 // });
-
+gulp.task('serve', [], function () {
+    browserSync.init({
+        server:{
+            baseDir: "./"
+        },
+        port : 5000
+    });
+    gulp.watch(import_style.my_css, ['css']);
+    gulp.watch(import_style.my_js, ['js']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
 gulp.task('watch', function () {
     gulp.watch(import_style.my_css, ['css']);
     gulp.watch(import_style.my_js, ['js']);
 });
 
-gulp.task('default', ['css', 'js']);
+gulp.task('default', ['css', 'js','serve']);
